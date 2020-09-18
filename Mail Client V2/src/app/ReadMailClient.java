6b3d11ff5540b9c86a0670e39ff555c345a74262
unknown <kovacevicnemanja97@gmail.com>
2020-09-18 21:34:56 +0200

    Zamenjen client_secret.json file

--- Mail Client V2/src/app/ReadMailClient.java
+++ Mail Client V2/src/app/ReadMailClient.java
@@ -1,8 +1,6 @@
 package app;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.InvalidAlgorithmParameterException;
@@ -46,8 +44,6 @@ public class ReadMailClient extends MailClient {
 
 	public static long PAGE_SIZE = 3;
 	public static boolean ONLY_FIRST_PAGE = true;
-
-	private static final String USERB_KEYSTORE = "./data/userb-keystore.jks";
 	
 	public static void main(String[] args) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, IllegalBlockSizeException, BadPaddingException, MessagingException, NoSuchPaddingException, InvalidAlgorithmParameterException, KeyStoreException, CertificateException, UnrecoverableKeyException {
 		BasicConfigurator.configure();
